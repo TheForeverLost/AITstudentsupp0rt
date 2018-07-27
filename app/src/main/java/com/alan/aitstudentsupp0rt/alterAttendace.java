f4b30f52e953cc83a9bb736eb1b086975e86ee90
TheForeverLost <alanpjohn@outlook.com>
2018-07-27 23:44:14 +0530

    just added check condition on open activity

---
+++ app/src/main/java/com/alan/aitstudentsupp0rt/alterAttendace.java
@@ -0,0 +1,13 @@
+package com.alan.aitstudentsupp0rt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class alterAttendace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alter_attendace);
+    }
+}
--- app/src/main/java/com/alan/aitstudentsupp0rt/open.java
+++ app/src/main/java/com/alan/aitstudentsupp0rt/open.java
@@ -5,7 +5,6 @@ import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.database.sqlite.SQLiteDatabase;
-import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -29,18 +28,28 @@ public class open extends AppCompatActivity {
             db=this.openOrCreateDatabase("AITstudent",MODE_PRIVATE,null);
             db.execSQL("CREATE TABLE IF NOT EXISTS users(name VARCHAR,passordBool INT(1) DEFAULT 0,password varchar ,logtime timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL)");
             try{
-                db.execSQL("DELETE FROM users");
+                c = db.rawQuery("Select * from users",null);
+                if(c!=null) {
+                    c.moveToFirst();
+                    int nameindex=c.getColumnIndex("name");
+                    String name=c.getString(nameindex);
+                    message.setText("hi "+name);
+                    rep.setText("click next to continue");
+                    rep.setEnabled(false);
+                    choice=1;
+                }else{
+                    message.setText("Hi enter your name");
+                    rep.setText("");
+                    choice = 2;
+                    Log.i("GG", "Choice = 2");
+                }
             }catch(Exception e){
-
+                Log.i("GG", e.getMessage());
             }
-            message.setText("Hi enter your name");
-            rep.setText("");
-            choice = 2;
-            Log.i("GG", "Choice = 2");
+
 
 
         }catch (Exception e){
-            Toast.makeText(this , "on create error" , Toast.LENGTH_LONG);
             Log.i("GG", "exception ran");
         }
 
@@ -48,10 +57,14 @@ public class open extends AppCompatActivity {
 
     protected void next(View view){
         Log.i("GG", Integer.toString(choice) );
-
+        Intent b;
         switch (choice){
             case 1:
-                    Toast.makeText(this , "already in database" , Toast.LENGTH_LONG);
+                Toast.makeText(this , "already in database" , Toast.LENGTH_LONG).show();
+                b=new Intent(com.alan.aitstudentsupp0rt.open.this,alterAttendace.class);
+                startActivity(b);
+                break;
+
             case 2:
                 String user=rep.getText().toString();
                 db.execSQL("INSERT INTO users(name) VALUES('"+ user +"')");
@@ -64,8 +77,8 @@ public class open extends AppCompatActivity {
                 break;
             case 4:
 
-                Toast.makeText(this , "added to database" , Toast.LENGTH_LONG);
-                Intent b=new Intent(com.alan.aitstudentsupp0rt.open.this,home_page.class);
+                Toast.makeText(this , "added to database" , Toast.LENGTH_LONG).show();
+                b=new Intent(com.alan.aitstudentsupp0rt.open.this,home_page.class);
                 startActivity(b);
                 break;
 
