5f1114ba2905eb84132e82334e966c297e60cdf5
TheForeverLost <alanpjohn@outlook.com>
2018-07-28 00:18:13 +0530

    adds declaration and data members of attendancePolicy class

---
+++ app/src/main/java/com/alan/aitstudentsupp0rt/AttendancePolicy.java
@@ -0,0 +1,29 @@
+package com.alan.aitstudentsupp0rt;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class AttendancePolicy {
+    boolean subjectwise ;
+    boolean physical;
+    boolean total;
+    boolean PL;
+    int subLimit;
+    int totalLimit;
+    int physicalLimit;
+    int PLlimit;
+    SQLiteDatabase db;
+
+    AttendancePolicy(){
+        subjectwise = true;
+        physical = true;
+        total = true;
+        PL = true;
+        subLimit = 0;
+        totalLimit = 0;
+        physicalLimit = 0;
+        PLlimit = 0;
+        
+    }
+
+
+}
