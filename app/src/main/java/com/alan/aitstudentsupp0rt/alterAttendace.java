e8e310aeb0cb61c47bca56f84723528f90f14262
TheForeverLost <alanpjohn@outlook.com>
2018-08-09 02:57:54 +0530

    repaired and perfected alterAttendance v1

--- app/src/main/java/com/alan/aitstudentsupp0rt/alterAttendace.java
+++ app/src/main/java/com/alan/aitstudentsupp0rt/alterAttendace.java
@@ -32,14 +32,17 @@ public class alterAttendace extends AppCompatActivity {
         edPL=(EditText) findViewById(R.id.editText5);
         cbPL=(CheckBox) findViewById(R.id.checkBox4);
         edTotal=(EditText) findViewById(R.id.editText2);
+        atp = new AttendancePolicy();
 
         try{
         db = openOrCreateDatabase("AITstudent",MODE_PRIVATE,null);
         db.execSQL("CREATE TABLE IF NOT EXISTS policy(total int(3),subjectwise int(3) DEFAULT 0,physical int(3) DEFAULT 0" +
                     ",PL int(3) DEFAULT 0)");
-
-        c = db.rawQuery("SELECT * FROM policy",null);
+        load();
+        /*c = db.rawQuery("SELECT * FROM policy",null);
+            Log.i("alter", c.getCount()+"");
         if(c.getCount()>0){
+            //Toast.makeText(this,"yes",Toast.LENGTH_SHORT).show();
             c.moveToLast();
             atp.obtain(c);
 
@@ -62,7 +65,7 @@ public class alterAttendace extends AppCompatActivity {
                 edphy.setAlpha(0f);
                 edphy.setText("");
             }
-            cbsub.setChecked(atp.physical);
+            cbphy.setChecked(atp.physical);
 
             if(atp.PL){
                 edPL.setAlpha(1f);
@@ -71,12 +74,16 @@ public class alterAttendace extends AppCompatActivity {
                 edPL.setAlpha(0f);
                 edPL.setText("");
             }
-            cbsub.setChecked(atp.physical);
+            cbPL.setChecked(atp.PL);
 
-            Log.i("GE", "SO It works too");
+            Log.i("alter", "SO It works too");
+        }else{
+            db.execSQL("INSERT INTO policy(total) VALUES(100)");
+        }*/
+
+        }catch(SQLException e){
+            Log.i("alter", e.getMessage());
         }
-
-        }catch(SQLException e){}
     }
 
     protected void phyClicked(View view){
@@ -110,65 +117,119 @@ public class alterAttendace extends AppCompatActivity {
         }
     }
 
+    protected void load(){
+        c = db.rawQuery("SELECT * FROM policy",null);
+        Log.i("alter", c.getCount()+"");
+        if(c.getCount()>0){
+            //Toast.makeText(this,"yes",Toast.LENGTH_SHORT).show();
+            c.moveToLast();
+            atp.obtain(c);
+
+            edTotal.setText(Integer.toString(atp.totalLimit));
+
+            if(atp.subjectwise){
+                edsub.setAlpha(1f);
+                edsub.setText(Integer.toString(atp.subLimit));
+
+            }else{
+                edsub.setAlpha(0f);
+                edsub.setText("");
+            }
+            cbsub.setChecked(atp.subjectwise);
+
+            if(atp.physical){
+                edphy.setAlpha(1f);
+                edphy.setText(Integer.toString(atp.physicalLimit));
+            }else{
+                edphy.setAlpha(0f);
+                edphy.setText("");
+            }
+            cbphy.setChecked(atp.physical);
+
+            if(atp.PL){
+                edPL.setAlpha(1f);
+                edPL.setText(Integer.toString(atp.PLlimit));
+            }else{
+                edPL.setAlpha(0f);
+                edPL.setText("");
+            }
+            cbPL.setChecked(atp.PL);
+
+            Log.i("alter", "SO It works too");
+        }else{
+            db.execSQL("INSERT INTO policy(total) VALUES(100)");
+        }
+    }
+
     protected void save(View view){
         //db.execSQL("DROP TABLE policy;");
-        int flag=1;
-        int totallimit = Integer.parseInt(edTotal.getText().toString());
-        Log.i("nps", String.valueOf(totallimit));
-        if(totallimit<100&&totallimit>0){
-            //atp.settotalLimit(totallimit);
-        }else{
-            flag=0;
-        }
-        int physical;
-        if(cbphy.isChecked()){
-            physical= Integer.parseInt(edphy.getText().toString());
-            if (physical<100&&physical>0){
-                //atp.setphysicalLimit(physical);
-            }else{
-                flag=0;
-            }
-        }else{
-            physical =0;
-        }
-        int subject;
-        if(cbsub.isChecked()){
-            subject= Integer.parseInt(edsub.getText().toString());
-            if (subject<100&&subject>0){
-                //atp.setsubLimit(com.alan.aitstudentsupp0rt.subject);
-            }else{
-                flag=0;
-            }
-        }else{
-            subject = 0;
-        }
 
-        int PLc;
-        if(cbPL.isChecked()){
-            PLc= Integer.parseInt(edPL.getText().toString());
-            if (PLc>0){
-                //atp.setPLlimit(PLc);
-            }else{
-                flag=0;
+        try {
+            int flag = 1;
+            int totallimit = Integer.parseInt(edTotal.getText().toString());
+            Log.i("nps", String.valueOf(totallimit));
+            if (totallimit < 100 && totallimit > 0) {
+                //atp.settotalLimit(totallimit);
+            } else {
+                flag = 0;
             }
-        }else{
-            PLc=0;
-        }
+            int physical;
+            if (cbphy.isChecked()) {
+                physical = Integer.parseInt(edphy.getText().toString());
+                if (physical < 100 && physical > 0) {
+                    //atp.setphysicalLimit(physical);
+                } else {
+                    flag = 0;
+                }
+            } else {
+                physical = 0;
+            }
+            int subject;
+            if (cbsub.isChecked()) {
+                subject = Integer.parseInt(edsub.getText().toString());
+                if (subject < 100 && subject > 0) {
+                    //atp.setsubLimit(com.alan.aitstudentsupp0rt.subject);
+                } else {
+                    flag = 0;
+                }
+            } else {
+                subject = 0;
+            }
 
-        if(flag==0){
+            int PLc;
+            if (cbPL.isChecked()) {
+                PLc = Integer.parseInt(edPL.getText().toString());
+                if (PLc > 0) {
+                    //atp.setPLlimit(PLc);
+                } else {
+                    flag = 0;
+                }
+            } else {
+                PLc = 0;
+            }
+
+            if (flag == 0) {
+                clear();
+                load();
+                Toast.makeText(this, "Please input valid data", Toast.LENGTH_SHORT).show();
+
+            } else {
+                //String query=atp.executeUpdate();
+                String query = "UPDATE policy SET total = " + totallimit + " , " +
+                        "subjectwise = " + subject + " , " +
+                        "physical = " + physical + " ," +
+                        "PL = " + PLc + " ";
+                db.execSQL(query);
+                Log.i("alter", query);
+
+                Intent b = new Intent(com.alan.aitstudentsupp0rt.alterAttendace.this, com.alan.aitstudentsupp0rt.home_page.class);
+                startActivity(b);
+            }
+        }catch (Exception e){
             clear();
-            Toast.makeText(this,"Please input valid data" , Toast.LENGTH_SHORT).show();
+            load();
 
-        }else{
-            //String query=atp.executeUpdate();
-            String query="UPDATE policy SET total = "+totallimit+" , " +
-                    "subjectwise = "+subject+" , " +
-                    "physical = "+physical+" ," +
-                    "PL = "+PLc+" ";
-            db.execSQL(query);
-
-            Intent b=new Intent(com.alan.aitstudentsupp0rt.alterAttendace.this,com.alan.aitstudentsupp0rt.home_page.class);
-            startActivity(b);
+            Toast.makeText(this, "Please input valid data", Toast.LENGTH_SHORT).show();
         }
 
     }
